0a4db5a55b873239150be6af7a48ea82df67250c
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2015-10-20 13:59:30 +0200

    Added package informations in __init__

--- mongo_dynamic_fixture/__init__.py
+++ mongo_dynamic_fixture/__init__.py
@@ -1,3 +1,12 @@
+__title__ = 'mongo-dynamic-fixture'
+__version__ = '0.1.0'
+__author__ = 'Lou Marvin Caraig'
+__author_email__ = 'loumarvincaraig@gmail.com'
+__project_url__ = 'https://github.com/se7entyse7en/mongo-dynamic-fixture'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 Lou Marvin Caraig'
+
+
 from mongo_dynamic_fixture.facades import N
 from mongo_dynamic_fixture.facades import G
 
