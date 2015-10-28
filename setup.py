70989c4be4d05e097f51c75fe83af8bd9152f038
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2015-10-28 18:04:52 +0100

    Updated setup.py classifiers

--- setup.py
+++ setup.py
@@ -22,6 +22,11 @@ classifiers = [
     'License :: OSI Approved :: MIT License',
     'Natural Language :: English',
     'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
     'Topic :: Database',
     'Topic :: Software Development :: Testing',
 ]
